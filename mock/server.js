e086133e411ccee9a1e009c7ba129e72ad9ecf15
Cc <9247831980@qq.com>
2017-06-07 11:58:14 +0800

    third-

--- mock/server.js
+++ mock/server.js
@@ -3,24 +3,24 @@ let path=require('path');
 let app = express();
 app.listen(3000);
 
-let content = require('./content/content');
-//广告的接口
-app.get('/api/content', (req,res)=>{
-    res.send(content);
-});
 let notice = require('./notice/notice');
-//广告的接口
 app.get('/api/notice', (req,res)=>{
     res.send(notice);
 });
-app.post('/api/comment',(req,res)=>{
-    console.log(req.body);
-    res.send({msg:'ok'});
-});
 let js = require('./js/js');
-app.get('/api/js', (req,res)=>{
+app.get('/api/js/:hash?', (req,res)=>{
     res.send(js);
 });
+let css = require('./css/css');
+app.get('/api/css/:hash?', (req,res)=>{
+    res.send(css);
+});
+let content = js;
+app.get('/api/content', (req,res)=>{
+    res.send(content);
+});
+
+
 app.get('/', (req,res)=>{
     res.sendFile(path.resolve('../dist/index.html'));
 });
